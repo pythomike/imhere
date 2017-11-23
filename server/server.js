0d7d6afa807e5773180bddde98f6220f9b9e1fe1
Mike Morganti <pythomike@gmail.com>
2017-11-23 14:10:25 -0800

    removed knexfile from server DIR, updated variable names in server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const ENV         = process.env.ENV || "development";
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const app         = express();
-const knexConfig  = require("./knexfile");
+const knexConfig  = require("../knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const knexLogger  = require('knex-logger');
 
