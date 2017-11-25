ac046093aba5a86173e01a7afd91d9eed1c6afa2
ShaneRichman <shane.m.richman@gmail.com>
2017-11-25 15:31:12 -0800

    minor deletion

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cookieSession({
   name: 'session',
   keys: ["key1,", "key2"]
 }))
-// app.use(cors())
+
 app.use(bodyParser.urlencoded({ extended:false }))
 app.use(bodyParser.json())
 
@@ -187,10 +187,6 @@ app.use(passport.session());
   app.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/login' }),
     function(req, res) {
-      console.log('hit')
-      res.set({
-        'Access-Control-Allow-Origin': '*'
-      })
       console.log("google login worked")
       // Successful authentication, redirect home.
       res.redirect('/');
