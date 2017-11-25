53468dcd0a10d3ed5f457e40a07d5c0c999fe2c9
ShaneRichman <shane.m.richman@gmail.com>
2017-11-25 05:06:15 -0800

    set up html for recaptcha

--- src/Navbar/Register.jsx
+++ src/Navbar/Register.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-
-// Email verification, captcha, change login button to logout 
+const Recaptcha = require('react-recaptcha');
+// Email verification, captcha, change login button to logout
 // if real email not entered, return error?
 // make all fields necessary, except phone call
 // make phone number only 10 digits long
@@ -51,58 +51,60 @@ class Register extends Component {
       <div>
         <h4>Most exciting REGISTER form</h4>
         <form onSubmit = {this.handleFormSubmit}>
-          <label> 
-            First Name: 
-            <input type        = "text" 
-                   name        = "first_name" 
-                   value       = {first_name} 
-                   onChange    = {this.onChange} 
+          <label>
+            First Name:
+            <input type        = "text"
+                   name        = "first_name"
+                   value       = {first_name}
+                   onChange    = {this.onChange}
                    placeholder = "Bobby" />
           </label> <br />
 
           <label>
-            Last Name: 
-            <input type        = "text" 
-                   name        = "last_name" 
-                   value       = {last_name} 
-                   onChange    = {this.onChange} 
+            Last Name:
+            <input type        = "text"
+                   name        = "last_name"
+                   value       = {last_name}
+                   onChange    = {this.onChange}
                    placeholder = "Sloppy" />
           </label> <br />
 
           <label>
-            Email Address: 
-            <input type        = "email" 
-                   name        = "email" 
-                   value       = {email} 
-                   onChange    = {this.onChange} 
+            Email Address:
+            <input type        = "email"
+                   name        = "email"
+                   value       = {email}
+                   onChange    = {this.onChange}
                    placeholder = "sloppybobby@yahoo.com" />
           </label><br />
 
           <label>
-            Phone Number: 
-            <input type        = "text" 
-                   name        = "phone_number" 
-                   value       = {phone_number} 
-                   placeholder = "6048008135" 
+            Phone Number:
+            <input type        = "text"
+                   name        = "phone_number"
+                   value       = {phone_number}
+                   placeholder = "6048008135"
                    onChange    = {this.handlePhoneNum} />
           </label><br />
 
           <label>
             Password
-            <input type        = "password" 
-                   name        = "password" 
-                   value       = {password} 
+            <input type        = "password"
+                   name        = "password"
+                   value       = {password}
                    onChange    = {this.onChange} />
           </label><br />
 
           <label>
             Password Confirmation
-            <input type        = "password" 
-                   name        = "password_confirmation" 
-                   value       = {password_confirmation} 
+            <input type        = "password"
+                   name        = "password_confirmation"
+                   value       = {password_confirmation}
                    onChange    = {this.onChange} />
           </label><br />
-
+          <Recaptcha
+              sitekey="6LfNXToUAAAAAIRKWO5FA5x8IQeuHz_QPDcaPbal"
+          />
           <input type="submit" value="submit" />
         </form>
       </div>
