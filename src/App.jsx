978f852920d161864f371da8dddbd1b674087897
Ilinca <xipascu@gmail.com>
2017-11-28 19:28:21 -0800

    Site is responsive to diff screens.

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,12 @@ class App extends Component {
       <Router>
         <div className="bgimg">
           <div className="App">
-            <header>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <div className="allinc">
               <Modals />
-
               <div className="hero">
                 <Carousels />
               </div>
-            </header>
+            </div>
           </div>
         </div>
       </Router>
