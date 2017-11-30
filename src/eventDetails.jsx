8442b622a032018475ff5464751f33ef04a62d81
Ilinca <xipascu@gmail.com>
2017-11-30 15:22:08 -0800

    styling changes

--- src/eventDetails.jsx
+++ src/eventDetails.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import moment from 'moment';
 import clockp from './pictures/clock.png';
 import calendar from './pictures/cal.png';
+import map from './pictures/maps.png'
 
 class eventDetails extends Component {
 
@@ -16,11 +17,10 @@ class eventDetails extends Component {
           <p className="calp"><img className="calimg" src={calendar} /> Date: {moment(currentEvent.start_time).format("ddd MMM DD, YYYY").toString()}</p>
           <p className="clockp"> <img className="clockimg" src={clockp} />  Time: {moment(currentEvent.start_time).format("h:mmA").toString()} </p>
           <p className="event-desc">{currentEvent.description}</p>
-          <h4>{currentEvent.location}</h4>
 
-            <div className="googlemaps-button">
+            <div className="googlemaps">
               <a target="_blank" href={`https://www.google.com/maps/dir/?api=1&destination=${currentEvent.latitude},${currentEvent.longitude}`}>
-                <img src="https://i.imgur.com/0JAMrnL.png" width="35em" className="googleMaps-icon" alt="map-icon"></img>
+                <img className="gmap" src={map} width="35em" alt="map-icon"></img> <p className="locp">{currentEvent.location}</p>
               </a>
             </div>
           </div>
